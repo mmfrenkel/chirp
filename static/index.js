b24b851cc7d92a9bb301ca7c44aa6aba70de4f2c
mmfrenkel <megan.frenkel@gmail.com>
2019-07-14 12:32:57 -0400

    getting layout setup

--- static/index.js
+++ static/index.js
@@ -4,38 +4,86 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.channelTab').forEach(button => {
         button.onclick = openChannel;
     })
+
+    document.querySelector('#newChannelForm').onsubmit = () => {
+
+        // create new channel elements
+        const newChannelName = document.querySelector('#newChannelName').value;
+        createNewChannel(newChannelName);
+        createNewChannelTab(newChannelName);
+
+        // reset the form, don't reload
+        document.querySelector('#newChannelForm').reset();
+        return false;
+    }
+
     // Get the element with id="defaultOpen" and click on it
-    debugger;
     document.getElementById("defaultOpen").click();
 });
 
-// factory function for generating elements ()
-function channelFactory(type, attributes, ...children) {
-    const element = document.createElement(type);
 
-    for (attribute in attributes) {
-        element.setAttribute(attribute, attributes[attribute]);
-    }
+// HELPER FUNCTIONS
+
+// factory function for generating elements ()
+function createNewChannel(channelName) {
+
+    // create the div
+    const newChannel = document.createElement('div');
+    newChannel.setAttribute("class", "channel");
+    newChannel.setAttribute("id", channelName);
+    newChannel.style.display = "none";
+
+    // create the header
+    const header = document.createElement('h3');
+    const headerText = document.createTextNode(`Welcome to ${channelName}!`);
+    header.appendChild(headerText);
+    newChannel.appendChild(header);
+
+    // create the 'chat' holder (temp)
+    const chatContainer = document.createElement('div')
+    chatContainer.setAttribute("class", "container chat")
+    const intro = document.createElement('p');
+    intro.appendChild(document.createTextNode('Start your conversation...'));
+    chatContainer.appendChild(intro)
+    newChannel.appendChild(chatContainer);
+
+    // add back into the channel list
+    document.getElementById("channelContainer").appendChild(newChannel);
+}
+
+function createNewChannelTab(channelName) {
+
+    // create the button
+    const newTab = document.createElement('button');
+    newTab.onclick = openChannel;
+
+    // add attributes
+    newTab.setAttribute("class", "channelTab");
+    newTab.setAttribute("data-channelname", channelName);
+
+    // add text to button
+    const tabText = document.createTextNode(channelName);
+    newTab.appendChild(tabText);
+
+    document.getElementById("tabList").appendChild(newTab);
 }
 
 function openChannel() {
 
     // Make other panels display as none
-    debugger;
-    var channels = document.getElementsByClassName("channel");
-    for (var i = 0; i < channels.length; i++) {
-        channels[i].style.display = "none";
+    const channels = document.getElementsByClassName("channel");
+    for (let channel of channels) {
+        channel.style.display = "none";
     }
     // the data-channelName attribute of the button = the id of the content box to display
-    var channelName = this.dataset.channelname
-    debugger;
+    const channelName = this.dataset.channelname
     document.getElementById(channelName).style.display = "block";
 
     // change all buttons to inactive, except "this" button
-    var channelTabs = document.getElementsByClassName("channelTab");
-    for (var i = 0; i < channelTabs.length; i++) {
+    const channelTabs = document.getElementsByClassName("channelTab");
+    for (let tab of channelTabs) {
         // first, by default, switch to inactive
-        channelTabs[i].className = channelTabs[i].className.replace(" active", "");
+        tab.className = tab.className.replace(" active", "");
     }
     this.className += " active";
 }
