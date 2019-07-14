7ffa7b749723f1c91fc1ba6c69312be0d17a945f
mmfrenkel <megan.frenkel@gmail.com>
2019-07-14 13:39:41 -0400

    changing to use a template-factory approach for html insert

--- static/index.js
+++ static/index.js
@@ -24,47 +24,96 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // HELPER FUNCTIONS
 
+// this helper function was inspired by Kyle Shevlin
+// (How to Write Your Own JavaScript DOM Element Factory, kyleshelvin.com)
+function elementFactory(type, attributes, ...children) {
+    const newElement = document.createElement(type);
+
+    for (let attribute in attributes) {
+        newElement.setAttribute(attribute, attributes[attribute]);
+    }
+
+    if (children[0] !== null) {
+        for (let child of children) {
+            if (typeof child === 'string') newElement.appendChild(
+                document.createTextNode(child)
+            );
+            else newElement.appendChild(child);
+        }
+    }
+    return newElement;
+}
+
 // factory function for generating elements ()
 function createNewChannel(channelName) {
 
-    // create the div
-    const newChannel = document.createElement('div');
-    newChannel.setAttribute("class", "channel");
-    newChannel.setAttribute("id", channelName);
+    const newChannel = elementFactory (
+        'div',
+        {
+            'class': 'channel',
+            'id': channelName
+        },
+        elementFactory (
+            'h3',
+            {},
+            `Welcome to ${channelName}!`
+        ),
+        elementFactory (
+            'div',
+            {
+                'class': 'container chat'
+            },
+            elementFactory (
+                'p',
+                {},
+                'Start your conversation...'
+            )
+        ),
+        elementFactory(
+            'div',
+            {'class': 'channelFooter'},
+            elementFactory(
+                'form',
+                {'class': 'submitNewMessage'},
+                elementFactory(
+                    'input',
+                    {
+                        'class': 'form-control newMessage',
+                        'type': 'text',
+                        'placeholder': 'Write a message',
+                        'name': 'message'
+                    },
+                    null
+                ),
+                elementFactory(
+                    'input',
+                    {
+                        'class': 'btn',
+                        'type': 'submit',
+                        'value': 'Enter'
+                    },
+                    null
+                )
+            )
+        )
+    );
+    debugger;
     newChannel.style.display = "none";
-
-    // create the header
-    const header = document.createElement('h3');
-    const headerText = document.createTextNode(`Welcome to ${channelName}!`);
-    header.appendChild(headerText);
-    newChannel.appendChild(header);
-
-    // create the 'chat' holder (temp)
-    const chatContainer = document.createElement('div')
-    chatContainer.setAttribute("class", "container chat")
-    const intro = document.createElement('p');
-    intro.appendChild(document.createTextNode('Start your conversation...'));
-    chatContainer.appendChild(intro)
-    newChannel.appendChild(chatContainer);
-
-    // add back into the channel list
     document.getElementById("channelContainer").appendChild(newChannel);
 }
 
 function createNewChannelTab(channelName) {
 
-    // create the button
-    const newTab = document.createElement('button');
+    const newTab = elementFactory(
+        'button',
+        {
+            'class': 'channelTab',
+            'data-channelname': channelName
+        },
+        channelName
+    )
+    debugger;
     newTab.onclick = openChannel;
-
-    // add attributes
-    newTab.setAttribute("class", "channelTab");
-    newTab.setAttribute("data-channelname", channelName);
-
-    // add text to button
-    const tabText = document.createTextNode(channelName);
-    newTab.appendChild(tabText);
-
     document.getElementById("tabList").appendChild(newTab);
 }
 
